d61861a84bd5c324aa87085dd2484ec5e2af5d9b
joaoVelosoT <vitorvteixeira2023@gmail.com>
2024-08-21 16:25:00 -0300

    termino da atividade

---
+++ lib/view/minha_tela.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:minha_tela_basica/widgets/card_card.dart';
+
+class minha_tela extends StatelessWidget {
+  const minha_tela({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Cart",
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        centerTitle: true,
+      ),
+      body: const Column(
+        children: [
+          card_cart(),
+          card_cart(),
+          card_cart(),
+          card_cart(),
+          card_cart(),
+          card_cart(),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        
+        child: Container(
+          
+          color: Colors.green,
+          
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Go to Checkout", style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 20)),
+              Container(
+                margin: const EdgeInsets.only(left: 20),
+                child: const Text("\$9,0", style: TextStyle(color: Colors.white,fontSize: 25, backgroundColor: Color.fromRGBO(0, 0, 0, 950),height: 1.5),))
+              ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/card_card.dart
@@ -0,0 +1,75 @@
+// ignore: camel_case_types
+import 'package:flutter/material.dart';
+
+class card_cart extends StatelessWidget {
+  const card_cart({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 30, right: 30),
+      padding: const EdgeInsets.all(16),
+      decoration: const BoxDecoration(
+          border: Border(
+               bottom: BorderSide(width: 1))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Column(children: [Image.asset("assets/images/banana.png")]),
+              Container(
+                margin: const EdgeInsets.only(left: 40),
+                child: Column(
+                  children: [
+                    const Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Organic Banana",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "1kg, Price",
+                        
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(right: 10),
+                          child: Icon(Icons.remove, size: 40,)),
+                        const Text("1", style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+                        Container(
+                          margin: const EdgeInsets.only(left: 10),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(width: 1),
+                              borderRadius: BorderRadius.circular(20)),
+                            child: const Icon(Icons.add, size: 40,color: Colors.green,)))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            // color: Colors.red,
+            
+            height: 90,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Icon(Icons.close, size: 30,), Text("\$4.99",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
